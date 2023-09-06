805f93de230aed8634535a684230db53dd397776
nagibatorartem3000 <144228006+nagibatorartem3000@users.noreply.github.com>
2023-09-06 13:10:35 +0300

    Add files via upload

---
+++ 1234567.pas
@@ -0,0 +1,11 @@
+﻿program z3;
+var a,b,c,d:integer;
+begin 
+  Writeln('Введите число трехзначное');
+  read(a);
+  b:=a div 100;
+  c:=a mod 10 ;
+  d:=a mod 100 div 10;
+  writeln('перевертыж ',c,d,b);
+  
+end.
